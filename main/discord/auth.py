f48f2faa55c7b39f952bc3b8d90d34db6bf9f408
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-06-29 21:16:43 -0400

    Create auth.py

---
+++ main/discord/auth.py
@@ -0,0 +1,38 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def exchange_code(code):
+    data = {
+        "client_id": os.environ.get("CLIENT_ID"),
+        "client_secret": os.environ.get("CLIENT_SECRET"),
+        "grant_type": os.environ.get("GRANT_TYPE"),
+        "code": code,
+        "redirect_uri": os.environ.get("REDIRECT_URI"),
+        "scope": os.environ.get("SCOPE"),
+    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    response = requests.post(
+        "https://discord.com/api/oauth2/token", data=data, headers=headers
+    )
+    credentials = response.json()
+    access_token = credentials["access_token"]
+    # Get user info
+    user_response = requests.get(
+        "https://discord.com/api/users/@me",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    # Get guilds info
+    guilds_response = requests.get(
+        "https://discord.com/api/users/@me/guilds",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    # Return info
+    user = user_response.json()
+    guilds = guilds_response.json()
+    return [user, guilds]
