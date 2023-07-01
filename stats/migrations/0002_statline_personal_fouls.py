5f5c0de4ab352fb941887619ef4aeb3484e3d49a
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-01 12:22:47 -0400

    Update 0002_statline_personal_fouls.py

--- stats/migrations/0002_statline_personal_fouls.py
+++ stats/migrations/0002_statline_personal_fouls.py
@@ -11,9 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='statline',
-            name='personal_fouls',
-            field=models.PositiveSmallIntegerField(default=0, validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(10)]),
+        #
         ),
     ]
