23bda03c48e81f2228b9ecef0ff6480e1a5f5062
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-06 22:12:05 -0400

    Update settings.py

--- hoops/settings.py
+++ hoops/settings.py
@@ -35,7 +35,7 @@ DEBUG = os.getenv("DEBUG", "False") == "False"
 ALLOWED_HOSTS = [
     "localhost:8000",
     "sea-lion-app-rva8v.ondigitalocean.app",
-    "virtualbl.com",
+    "www.virtualbl.com",
     "159.203.104.106",
 ]
 
