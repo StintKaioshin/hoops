ba25f689080aa75573239ef8e86c38f76e542f25
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-16 17:16:48 -0400

    Update settings.py

--- hoops/settings.py
+++ hoops/settings.py
@@ -102,13 +102,16 @@ WSGI_APPLICATION = "hoops.wsgi.application"
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db-postgresql-nyc1-96273',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_7W4SjuocqCmSEg_ye7V ',
+        'HOST': 'db-postgresql-nyc1-96273-do-user-14309916-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
     }
+}
 elif len(sys.argv) > 0 and sys.argv[1] != "collectstatic":
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not set")
