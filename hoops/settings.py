943e16c714677d67453c5a78d4dba35e7eb81df8
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-16 13:16:28 -0400

    Update settings.py

--- hoops/settings.py
+++ hoops/settings.py
@@ -20,8 +20,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = [BASE_DIR / 'static']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
