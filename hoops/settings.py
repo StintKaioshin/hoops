3977ad4082fb23f67330ea6f4140994690153f11
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-17 13:30:08 -0400

    Allowed host change

--- hoops/settings.py
+++ hoops/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
     "159.203.104.106",
     "sea-lion-app-rva8v.ondigitalocean.app",
     "virtualbl.com",
-    "www.virtualbl.com",
+    "localhost",
 ]
 
 AUTHENTICATION_BACKENDS = [
