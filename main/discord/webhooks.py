72088e01b72b581afab4d58218426b15ffee479b
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-20 21:13:09 -0400

    Update webhooks.py

--- main/discord/webhooks.py
+++ main/discord/webhooks.py
@@ -1,5 +1,14 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+webhook_urls = {
+    "upgrade": "https://discord.com/api/webhooks/1131755543451668560/DFXdYbF8_sz-8VoXT23AbEyi_NTDk3HK-fF3sgpCWLdf2TP7hxelqKLp90Xc8tpy6X8Y",
+    "creation": "https://discord.com/api/webhooks/1131755543451668560/DFXdYbF8_sz-8VoXT23AbEyi_NTDk3HK-fF3sgpCWLdf2TP7hxelqKLp90Xc8tpy6X8Y",
+    "coupon": "https://discord.com/api/webhooks/1131755543451668560/DFXdYbF8_sz-8VoXT23AbEyi_NTDk3HK-fF3sgpCWLdf2TP7hxelqKLp90Xc8tpy6X8Y",
+    "cash": "https://discord.com/api/webhooks/1131755543451668560/DFXdYbF8_sz-8VoXT23AbEyi_NTDk3HK-fF3sgpCWLdf2TP7hxelqKLp90Xc8tpy6X8Y",
+    "trade": "https://discord.com/api/webhooks/1131755543451668560/DFXdYbF8_sz-8VoXT23AbEyi_NTDk3HK-fF3sgpCWLdf2TP7hxelqKLp90Xc8tpy6X8Y",
+    "style": "https://discord.com/api/webhooks/1131755543451668560/DFXdYbF8_sz-8VoXT23AbEyi_NTDk3HK-fF3sgpCWLdf2TP7hxelqKLp90Xc8tpy6X8Y",
+}
+
 default_webhook_msg = "Webhook test message"
 
 
@@ -9,7 +18,7 @@ def send_webhook(url, title="", message=""):
     # Create an embed
     webhook_embed = DiscordEmbed(title=title, description=message, color="03b2f8")
     webhook_embed.set_timestamp()
-    webhook_embed.set_footer(text="Powered by hoopsim.com")
+    webhook_embed.set_footer(text="Powered by virtualbl.com")
     # Add embed object to webhook
     webhook.add_embed(webhook_embed)
     # Send webhook
