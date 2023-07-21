1a5e676d59d16490befcbff8d0c3a1c07d103618
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-21 14:36:00 -0400

    Update views.py

--- main/views.py
+++ main/views.py
@@ -355,11 +355,17 @@ def create_player(request):
             'attribute_categories': attribute_categories,
             'badge_categories': badge_categories,
             'user': request.user
+                }
 
-
+            
+    
 def players(request):
-        }
         return render(request, "main/players/create.html", context)
+    
+    def players(request):
+        context = {
+            "title": "Players",
+    }
 # Get the league players
     league_players = Player.objects.order_by("id")
     # Paginate the league players
