f490b5d397d3385977d5b09201854cd6d425dca0
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-23 17:30:31 -0400

    Update create.py

--- main/league/player/create.py
+++ main/league/player/create.py
@@ -114,6 +114,12 @@ def createPlayer(user, formData):
         "secondary_badge5": formData.get("secondary_badge5", None),
     }
     # Save the player
+    for attribute in newPlayer.attributes:
+    new_attributes = position_attributes[newPlayer.primary_position]
+    newPlayer.attributes[attribute] = new_attributes[attribute]
+    # Update the player's starting physicals
+    updatedPlayer = league_physicals.setStartingPhysicals(newPlayer)
+    # Save the player
     historyList.save()
     newPlayer.save()
     # Return the player
