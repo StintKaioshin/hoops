f191278c16ef424d6f5e837447b181031b1bb95f
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-23 17:08:05 -0400

    Update create.py

--- main/league/player/create.py
+++ main/league/player/create.py
@@ -81,7 +81,6 @@ def createPlayer(user, formData):
         jersey_number=formData["jersey_number"],
         # Relationships
         discord_user=user,
-        history_list=historyList,
     )
     # Update the player's attributes
     newPlayer.attributes = {
