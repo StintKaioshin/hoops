a85b80fb6c813a5c8dbada6000c21d7173fbfc10
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-23 15:04:39 -0400

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,16 +1,46 @@
-# Custom imports
-import json
-
 # Django imports
-from django import forms
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Main imports
+# Custom imports
+from .managers import DiscordAuthorizationManager
 from .league import config as league_config
-from copy import deepcopy
 
-player_styles = open("main/league/looyh/styles.json")
-player_styles = json.load(player_styles)
+
+# DiscordUser Models
+class DiscordUser(models.Model):
+    # Custom Manager
+    objects = DiscordAuthorizationManager()
+    # Discord User Model
+    id = models.BigIntegerField(primary_key=True, serialize=False)
+    discord_tag = models.CharField(max_length=100)
+    avatar = models.CharField(max_length=100)
+    public_flags = models.IntegerField()
+    flags = models.IntegerField()
+    locale = models.CharField(max_length=100)
+    mfa_enabled = models.BooleanField()
+    last_login = models.DateTimeField(null=True)
+    last_reward = models.DateTimeField(null=True)
+    # Permissions
+    can_update_players = models.BooleanField(default=False)
+    can_approve_trades = models.BooleanField(default=False)
+    can_update_styles = models.BooleanField(default=False)
+    can_edit_events = models.BooleanField(default=False)
+    can_edit_scores = models.BooleanField(default=False)
+    # Player Slots
+    player_slots = models.SmallIntegerField(default=league_config.max_players)
+    auto_collect_rewards = models.BooleanField(default=False)
+    can_change_styles = models.BooleanField(default=False)
+
+    # Discord User Methods
+    def is_authenticated(self, request):
+        return True
+
+    def __str__(self):
+        return f"{self.discord_tag}"
+
+
+# Player Models
 class Player(models.Model):
     # Player Model
     first_name = models.CharField(default="Unknown", max_length=16)
@@ -98,56 +128,131 @@ class HistoryList(models.Model):
     history = models.JSONField(default=league_config.get_default_history, blank=True)
 
 
-class UpgradeForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(UpgradeForm, self).__init__(*args, **kwargs)
-        # For each key in attributes, create integerfield
-        for key in league_config.initial_attributes:
-            if key in league_config.attribute_categories["physical"]:
-                continue
-            self.fields[key] = forms.IntegerField(
-                label=key,
-                min_value=0,
-                max_value=league_config.max_attribute,
-                widget=forms.NumberInput(attrs={"onchange": "updatePrice()"}),
-            )
-        # For each key in badges, create choicefield
-        for key in league_config.initial_badges:
-            self.fields[key] = forms.ChoiceField(
-                label=key,
-                choices=league_config.badge_upgrade_choices,
-                widget=forms.Select(attrs={"onchange": "updatePrice()"}),
-            )
-        # For each key in tendencies, create integerfield
-        for key in league_config.initial_tendencies:
-            # If players cannot change this tendency, skip
-            if key in league_config.banned_tendencies:
-                continue
-            # If players can change this tendency, create field
-            self.fields[key] = forms.IntegerField(
-                label=key,
-                min_value=league_config.min_tendency,
-                max_value=league_config.max_tendency,
-                widget=forms.NumberInput(attrs={"onchange": "updatePrice()"}),
-            )
-        # For each key in hotzones, create choicefield
-        # for key in league_config.initial_hotzones:
-        #     self.fields[key] = forms.ChoiceField(
-        #         label=key,
-        #         choices=league_config.hotzone_choices,
-        #         widget=forms.Select(attrs={"onchange": "updatePrice()"}),
-        #     )
+# Team & Statistic Models
+class Team(models.Model):
+    # Team Model
+    name = models.CharField(max_length=32)
+    logo = models.CharField(max_length=100, default=league_config.initial_team_logo)
+    abbrev = models.CharField(max_length=3)
+    picks = models.JSONField(
+        default=league_config.get_default_picks, blank=True, null=True
+    )
+    plays_in_main_league = models.BooleanField(default=True)
+    show_on_lists = models.BooleanField(default=True)
+    # Relationships
+    manager = models.ForeignKey(
+        "DiscordUser", blank=True, null=True, on_delete=models.CASCADE
+    )
+    history_list = models.ForeignKey("HistoryList", on_delete=models.CASCADE)
 
-class StylesForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(StylesForm, self).__init__(*args, **kwargs)
-        for style, data in player_styles.items():
-            # Create the ChoiceField
-            self.fields[style] = forms.ChoiceField(
-                label=style,
-                choices=data["options"],
-                widget=forms.Select(),
-            )
+    # Team Methods
+    def __str__(self):
+        return f"{self.name}"
 
-class TradeForm(forms.Form):
-    pass
+
+# Currency Transaction Models
+class Transaction(models.Model):
+    # Transaction Model
+    transaction_type = models.CharField(
+        max_length=16,
+        choices=league_config.transaction_type_choices,
+        default=league_config.transaction_type_choices[0][0],
+    )
+    amount = models.PositiveBigIntegerField()
+    reason = models.CharField(max_length=100, blank=True)
+    # Relationships
+    giver = models.ForeignKey("DiscordUser", on_delete=models.CASCADE)
+    player = models.ForeignKey("Player", on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=False)
+
+    # Transaction Methods
+    def __str__(self):
+        return f"{self.transaction_type}: {self.amount}"
+
+
+# Trade Models
+class TradeOffer(models.Model):
+    # Trade Offer Model
+    offer = models.JSONField(default=league_config.get_default_trade_offer)
+    accepted = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
+    finalized = models.BooleanField(default=False)
+    notes = models.CharField(max_length=100, blank=True)
+    # Relationships
+    sender = models.ForeignKey("Team", on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(
+        "Team", on_delete=models.CASCADE, related_name="receiver"
+    )
+
+    # Trade Offer Methods
+    def __str__(self):
+        return f"{self.sender} -> {self.receiver}"
+
+
+# Offer Models
+class ContractOffer(models.Model):
+    # Contract Offer Model
+    years = models.PositiveSmallIntegerField()
+    salary = models.PositiveBigIntegerField()
+    option = models.CharField(
+        max_length=54,
+        choices=league_config.contract_option_choices,
+        default=league_config.contract_option_choices[0][0],
+    )
+    benefits = models.CharField(
+        max_length=54,
+        choices=league_config.contract_benefit_choices,
+        default=league_config.contract_benefit_choices[0][0],
+    )
+    notes = models.CharField(max_length=100, blank=True, null=True, default="No notes.")
+    # Relationships
+    team = models.ForeignKey("Team", on_delete=models.CASCADE)
+    player = models.ForeignKey("Player", on_delete=models.CASCADE)
+    # Boolean Fields
+    accepted = models.BooleanField(default=False)
+
+    # Contract Offer Methods
+    def __str__(self):
+        return f"{self.team.abbrev} -> {self.player.first_name} {self.player.last_name}"
+
+
+# Coupon Models
+class Coupon(models.Model):
+    code = models.CharField(max_length=16, unique=True)
+    name = models.CharField(max_length=16, default="Default Coupon")
+    amount = models.PositiveBigIntegerField()
+    one_use = models.BooleanField(default=False)
+    used = models.BooleanField(default=False)
+
+    # Coupon Methods
+    def __str__(self):
+        return f"{self.code}"
+
+
+# Notification Models
+class Notification(models.Model):
+    # Notifcation Model
+    title = models.CharField(max_length=32, default="Notification")
+    message = models.CharField(max_length=100, default="Content")
+    read = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=False)
+    # Relationships
+    discord_user = models.ForeignKey("DiscordUser", on_delete=models.CASCADE)
+
+
+# Award Models
+class Award(models.Model):
+    # Award Model
+    name = models.CharField(
+        max_length=6,
+        choices=league_config.award_name_choices,
+        default=league_config.award_name_choices[0][0],
+    )
+    description = models.CharField(max_length=16, default="Description")
+    # Relationships
+    player = models.ForeignKey("Player", on_delete=models.CASCADE)
+    season = models.PositiveSmallIntegerField(default=1)
+
+    # Award Methods
+    def __str__(self):
+        return f"S{self.season} - {self.name} - {self.player}"
