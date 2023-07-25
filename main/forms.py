b93059ba60ecf305821330192c3c6d01aaa99ba1
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-07-24 23:15:21 -0400

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -21,7 +21,6 @@ class PlayerForm(forms.Form):
         label="Jersey Number", min_value=0, max_value=league_config.max_attribute
     )
     referral_code = forms.CharField(label="Referral Code", required=False, max_length=16)
-
     def __init__(self, *args, **kwargs):
         attribute_categories = kwargs.pop('attribute_categories', None)
         badge_categories = kwargs.pop('badge_categories', None)
@@ -44,38 +43,38 @@ class PlayerForm(forms.Form):
             self.fields[f'secondary_attr{i}'] = forms.ChoiceField(choices=attribute_choices, required=True)
             self.fields[f'secondary_badge{i}'] = forms.ChoiceField(choices=badge_choices, required=False)
 class UpgradeForm(forms.Form):
+    # Your UpgradeForm fields here...
     def __init__(self, *args, **kwargs):
         super(UpgradeForm, self).__init__(*args, **kwargs)
-
+        # For each key in attributes, create integerfield
         for key in league_config.initial_attributes:
             if key in league_config.attribute_categories["physical"]:
                 continue
-            # Add prefix to distinguish fields
-            self.fields[f"attribute_{key}"] = forms.IntegerField(
+            self.fields[key] = forms.IntegerField(
                 label=key,
                 min_value=0,
                 max_value=league_config.max_attribute,
                 widget=forms.NumberInput(attrs={"onchange": "updatePrice()"}),
             )
-        
+        # For each key in badges, create choicefield
         for key in league_config.initial_badges:
-            # Add prefix to distinguish fields
-            self.fields[f"badge_{key}"] = forms.ChoiceField(
+            self.fields[key] = forms.ChoiceField(
                 label=key,
                 choices=league_config.badge_upgrade_choices,
                 widget=forms.Select(attrs={"onchange": "updatePrice()"}),
             )
-
+        # For each key in tendencies, create integerfield
         for key in league_config.initial_tendencies:
+            # If players cannot change this tendency, skip
             if key in league_config.banned_tendencies:
                 continue
-            # Add prefix to distinguish fields
-            self.fields[f"tendency_{key}"] = forms.IntegerField(
+            # If players can change this tendency, create field
+            self.fields[key] = forms.IntegerField(
                 label=key,
                 min_value=league_config.min_tendency,
                 max_value=league_config.max_tendency,
                 widget=forms.NumberInput(attrs={"onchange": "updatePrice()"}),
-            )
+             )
         # For each key in hotzones, create choicefield
         # for key in league_config.initial_hotzones:
         #     self.fields[key] = forms.ChoiceField(
