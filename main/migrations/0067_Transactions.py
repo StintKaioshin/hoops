974e32a774a2a8d63e1dcd6fa853b441d6f70493
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-08-04 14:56:31 -0400

    Update 0067_Transactions.py

--- main/migrations/0067_Transactions.py
+++ main/migrations/0067_Transactions.py
@@ -1,14 +1,19 @@
-from django.db import migrations, models
+from django.db import migrations
+from main.models import Team
+
+def handle_null_from_team(apps, schema_editor):
+    TransactionsEtc = apps.get_model('main', 'TransactionsEtc')
+    default_team = Team.objects.first()  # or get a team in some other way
+
+    # Assign the default team to transactions with NULL from_team_id
+    TransactionsEtc.objects.filter(from_team__isnull=True).update(from_team=default_team)
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0066_player_trait_three'),
+        ('main', '0066_player_trait_three'),  # replace with the actual previous migration name
     ]
 
     operations = [
-    migrations.RunSQL(
-        'ALTER TABLE main_transactionsetc ALTER COLUMN from_team_id DROP NOT NULL',
-        'ALTER TABLE main_transactionsetc ALTER COLUMN from_team_id SET NOT NULL'
-    )
+        migrations.RunPython(handle_null_from_team),
     ]
