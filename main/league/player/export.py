d76d2d23186c9e6326cccb1b886cf63febf5d518
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-09-14 17:09:38 -0400

    Update export.py

--- main/league/player/export.py
+++ main/league/player/export.py
@@ -270,9 +270,8 @@ def export_player(player):
         # We don't need to format these, they are already formatted
         # We do, however, need to convert the values to strings
         # In other words, if the player is using website/database tendencies
-        if not player.use_game_tendencies:
-            for tendency, value in database_tendencies.items():
-                game_file[4]["data"][tendency] = str(value)
+        for tendency, value in database_tendencies.items():
+            game_file[4]["data"][tendency] = str(value)
 
     # Set the player's statics
     def set_statics():
