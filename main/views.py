943cf254383de61d734064773fed9ff6a0682e98
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-10-10 11:08:29 -0400

    Update views.py

--- main/views.py
+++ main/views.py
@@ -409,6 +409,8 @@ def upgrade_player(request, id):
         "defense_badges": league_config.badge_categories["defense"],
         # Tendency categories
         "initial_tendencies": league_config.initial_tendencies,
+        "initial_hotzones": league_config.initial_hotzones,
+
     }
     return render(request, "main/players/upgrade.html", context)
 import logging
