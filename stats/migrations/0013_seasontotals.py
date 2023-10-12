728c7adea16be8056010a486d27672326f1847cc
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-10-12 12:21:00 -0400

    Update 0013_seasontotals.py

--- stats/migrations/0013_seasontotals.py
+++ stats/migrations/0013_seasontotals.py
@@ -7,6 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('main', '0065_team_show_on_lists'), 
         ('stats', '0012_seasonaverage_average_type'),
     ]
 
