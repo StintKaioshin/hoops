8ce2f04e74f63d81ddee9d9b0a35013923bdd566
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-10-12 12:24:56 -0400

    Update 0009_seasonaverages.py

--- stats/migrations/0009_seasonaverages.py
+++ stats/migrations/0009_seasonaverages.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0065_team_show_on_lists'),
+        ('main', '0059_alter_contractoffer_notes'),
         ('stats', '0008_alter_game_game_type'),
     ]
 
