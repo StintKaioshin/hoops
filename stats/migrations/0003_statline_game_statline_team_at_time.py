dda4d3a1209be1c97223dcfe2c224df8b751b70d
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-10-12 12:20:30 -0400

    Update 0003_statline_game_statline_team_at_time.py

--- stats/migrations/0003_statline_game_statline_team_at_time.py
+++ stats/migrations/0003_statline_game_statline_team_at_time.py
@@ -7,6 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('main', '0064_discorduser_can_update_styles_alter_player_styles'), 
         ('stats', '0002_statline_personal_fouls'),
     ]
 
