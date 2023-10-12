02cfa1a69a3717172206901620d5288766b9e622
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-10-12 12:21:38 -0400

    Update 0017_alter_seasonaverage_season_alter_seasontotal_season_and_more.py

--- stats/migrations/0017_alter_seasonaverage_season_alter_seasontotal_season_and_more.py
+++ stats/migrations/0017_alter_seasonaverage_season_alter_seasontotal_season_and_more.py
@@ -6,6 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('main', '0065_team_show_on_lists'),
         ('stats', '0016_remove_seasontotal_gmsc'),
     ]
 
