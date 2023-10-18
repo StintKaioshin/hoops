f8147fe5b78a0be6d8e00ea5331344779b4aa47a
StintKaioshin <137710222+StintKaioshin@users.noreply.github.com>
2023-10-18 14:25:47 -0400

    Update copyJSON.js

--- main/static/main/js/players/copyJSON.js
+++ main/static/main/js/players/copyJSON.js
@@ -1,22 +1,15 @@
-copyButtons = document.getElementsByClassName("copyJSON");
-
 function clipboard(clicked_id) {
-    // Derive the corresponding <p> ID based on the button's ID
-    let fileID = 'json_' + clicked_id;
-
-    let btn = document.getElementById(clicked_id);
+    // Correctly generate the ID for the paragraph element
+    let fileID = 'json_' + clicked_id.split("_")[1];
+    
+    // Get the corresponding paragraph element
     let file = document.getElementById(fileID);
-
-    // Check if the file element exists (this is a simple debug step, can be removed later)
-    if (!file) {
-        alert('File element not found for ID: ' + fileID);
-        return;
-    }
 
     // Copy the file content to the clipboard
     navigator.clipboard.writeText(file.innerText);
 
-    // Update the button's text and style
+    // Update button text and style
+    let btn = document.getElementById(clicked_id);
     btn.innerText = "Copied!";
     btn.classList.remove("btn-primary");
     btn.classList.add("btn-success");
